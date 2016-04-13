baf874ef6adb3c6565b25de0831668de5a1e6815
NOKUBI Takatsugu <takatsugu.nokubi@toppan.co.jp>
2016-04-13 18:01:56 +0900

    Initial commit

---
+++ colored-relation.rb
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+Plugin.create(:colored_relation) do
+  UserConfig[:followed_users_background_colod] ||= [45900, 62955, 36776]
+  UserConfig[:protected_users_background_color] ||= [0xcccc,0xcccc,0xcccc]
+
+  filter_message_background_color do | mp, array |
+    if mp.to_message.user[:protected] == true
+      array = UserConfig[:protected_users_background_color]
+    else
+      result = Service.any? { |me|
+        x = Plugin[:followingcontrol].relation.followers[me.user_obj]
+        if x && x.any? { |you| you == mp.to_message.user }
+          array = UserConfig[:followed_users_background_color]
+        end
+      }
+    end
+    [mp, array]
+  end
+  
+  settings("Users_Relation_Color") do
+    color("鍵付きユーザの背景色",:protected_users_background_color)
+    color("フォローされているユーザの背景色",:followed_users_background_color)
+  end
+  
+end
